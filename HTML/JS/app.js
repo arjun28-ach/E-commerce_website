dfbcec6c492003d28673fa3d7f13fd561ac715fe
Arjun Acharya <acharayaarjun28@gmail.com>
2021-06-25 20:51:18 +0545

    Products Page Complete

---
+++ HTML/JS/app.js
@@ -0,0 +1,39 @@
+
+ //-------------------------------Initialize Swiper Carousel------------------------------------
+
+var swiper = new Swiper(".mySwiper", {
+    effect: "coverflow",
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: "auto",
+    coverflowEffect: {
+      rotate: 50,
+      stretch: 0,
+      depth: 100,
+      modifier: 1,
+      slideShadows: true,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+    },
+  });
+    
+
+
+
+        //-------------------------------js for toogle menu------------------------------------
+
+    var MenuItems = document.getElementById("MenuItems");
+    MenuItems.style.maxHeight == "0px";
+
+    function menutoggle(){
+        if(MenuItems.style.maxHeight == "0px")
+        {
+            MenuItems.style.maxHeight = "200px";
+        }
+        else
+        {
+            MenuItems.style.maxHeight = "0px";
+        }
+    }
+      
